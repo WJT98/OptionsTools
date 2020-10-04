9afbb5bbc3b620701f574a56be1fc3cd7afe0cc7
wjt98 <williamjt1998@gmail.com>
2020-10-03 21:18:16 -0400

    Removed bootstrap for angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,27 +6,50 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { OptionsDataComponent } from './options-data/options-data.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 // Add Material
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DashboardComponent } from './dashboard/dashboard.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatSelectModule } from '@angular/material/select';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+import { MatRippleModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+
+
+
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    OptionsDataComponent,
-    SidebarComponent,
-    DashboardComponent,
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FlexLayoutModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        OptionsDataComponent,
+        SidebarComponent,
+        DashboardComponent,
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FlexLayoutModule,
+        BrowserAnimationsModule,
+        MatFormFieldModule,
+        MatSelectModule,
+        MatToolbarModule,
+        MatInputModule,
+        MatIconModule,
+        MatSidenavModule,
+        MatDividerModule,
+        MatListModule,
+        MatRippleModule,
+        MatButtonModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
