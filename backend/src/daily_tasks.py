4cd45b0c78bc28e99e3c716de3db77422d955575
wjt98 <williamjt1998@gmail.com>
2020-12-27 23:00:29 -0500

    Added delay to OIC requests

--- backend/src/daily_tasks.py
+++ backend/src/daily_tasks.py
@@ -1,5 +1,4 @@
 import psycopg2
-import scrape_oic as so
 import logging
 from datetime import date
 import os
@@ -12,6 +11,11 @@ import csv
 from psycopg2 import OperationalError, errorcodes, errors
 import traceback
 import sys
+import time
+import scrape_oic as so
+
+DEBUG = False
+
 
 def init_child(lock_):
 	global lock
@@ -22,7 +26,7 @@ def init_child(lock_):
 
 def update_options_chain(conn):
 	try:
-		cursor = conn.cursor()
+		cur = conn.cursor()
 		q = '''INSERT INTO options_chain (underlying_id, exp_date, strike, option) 
 				SELECT DISTINCT t.id, im.exp_date, 
 					im.strike, im.option
@@ -35,17 +39,17 @@ def update_options_chain(conn):
 										AND oc.exp_date = im.exp_date
 										AND oc.strike = im.strike
 										AND oc.option = im.option);'''
-		cursor.execute(q)
+		cur.execute(q)
 		conn.commit()
 	except Exception as err:
 		raise err
 	finally:
-		if cursor: 
-			cursor.close()
+		if cur: 
+			cur.close()
 
 def update_options_metrics(conn):
 	try:
-		cursor = conn.cursor()
+		cur = conn.cursor()
 		q = '''INSERT INTO options_metrics (option_id, val_date, bid, ask,
 					volume, open_interest, iv, delta, gamma, theta, alpha,
 					 vega, rho) 
@@ -57,50 +61,51 @@ def update_options_metrics(conn):
 					AND im.exp_date = oc.exp_date
 					AND im.strike = oc.strike
 					AND im.option = oc.option
+					AND im.ticker = t.ticker
 					AND NOT EXISTS (SELECT * 
 									FROM options_metrics AS om, 
-										options_chain AS oc, imports AS im
-									WHERE om.option_id = oc.id
+										options_chain AS oc, imports AS im, tickers AS t
+									WHERE t.id = oc.underlying_id
+										AND om.option_id = oc.id
 										AND om.val_date = im.val_date
 										AND im.exp_date = oc.exp_date);'''
-		cursor.execute(q)
+		cur.execute(q)
 		conn.commit()
 	except Exception as err:
 		raise err
 	finally:
-		if cursor: 
-			cursor.close()
+		if cur: 
+			cur.close()
 		
 
 def truncate_imports(conn):
 	try:
-		cursor = conn.cursor()
-		cursor.execute('TRUNCATE TABLE imports;')
+		cur = conn.cursor()
+		cur.execute('TRUNCATE TABLE imports;')
+		cur.close()
 	except Exception as err:
 		raise err
-	finally:
-		if cursor:
-			cursor.close()
 
-def import_table(csv_file, ticker, cursor):
+def import_table(csv_file, ticker, cur):
 	try:
 		headers = ['strike', 'option', 'ticker', 'bid', 'ask', 'volume', 
 		'open_interest', 'iv', 'delta', 'gamma', 'Theta', 'alpha', 'vega', 
 		'rho', 'exp_date', 'val_date']
-		with open(csv_file, 'r') as f:
-			cursor.copy_from(f, 'imports', columns=headers,sep=',')
+		with open(csv_file, 'r') as f: 
+			next(f)
+			cur.copy_from(f, 'imports', columns=headers,sep=',')
 	except Exception as err:
 		raise err
 
 def get_csv_list(ticker, vdate):
 	try:
 		path = "csv/" + ticker
-		directories = [path+'/'+d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
+		#directories = [path+'/'+d for d in os.listdir(path) if os.path.isdir(os.path.join(path, d))]
 		all_files = []
-		for d in directories:
-			files = [d+'/'+f for f in os.listdir(d) 
-						if (os.path.isfile(os.path.join(d,f)) and f == vdate+'.csv')]
-			all_files +=files
+		#for d in directories:
+		files = [path+"/"+f for f in os.listdir(path) 
+					if (os.path.isfile(os.path.join(path,f)) and f == vdate+'.csv')]
+		all_files +=files
 	except Exception as err:
 		raise err
 	return all_files
@@ -124,16 +129,24 @@ def mproc_job(ticker, vdate):
 		#with lock:
 		logging.info(ticker+": inserted into db + committed")
 		conn.commit()
+		
 	except Exception as err:
 		if conn is not None:
 			conn.rollback()
+		cursor = None
 		raise err
 	finally:
 		if conn:
-			cursor.close()
 			conn.close()
 		print(ticker, " ended")
 
+def scrape_htmls(tickers,vdate):
+	for ticker in tickers:
+		filename = os.path.join(os.getcwd(),"bs4_html/"+ticker[0]+"/"+vdate+".html")
+		if not os.path.exists(filename):
+			so.get_html(ticker[0], vdate)
+			time.sleep(30)
+	
 
 
 def main():
@@ -146,29 +159,32 @@ def main():
 							logging.FileHandler('logs/'+date.today().strftime("%Y-%m-%d") + '.log'),
 							logging.StreamHandler()])	
 
-
-	conn = db_setup.get_conn()
-	cur = conn.cursor()
 	try:
 		#cur.execute("""SELECT ticker, exchange FROM TICKERS""")
+		conn = db_setup.get_conn()
+		cur = conn.cursor()
 		cur.execute("""SELECT ticker FROM TICKERS""")
 		tickers = cur.fetchall()
-		tickers = [("TSLA",)]
+		truncate_imports(conn)
+		cur.close()
+		conn.close()
+		if DEBUG:
+			tickers = [("TSLA",)]
 		vdate = date.today().strftime("%Y-%m-%d")
-		pool_size = 1
+		pool_size = 5
 		lock = Lock()
-		print(f'starting computations on {pool_size} cores')
 		start = timer()
 		
 
 		#pool_size = cpu_count()
-	
-
-		truncate_imports(conn)
+		scrape_htmls(tickers, vdate)
+		print(f'starting computations on {pool_size} cores')
 		with Pool(pool_size, initializer=init_child,initargs=(lock,)) as pool:
 			args = [(t[0],vdate) for t in tickers]
 			pool.starmap(mproc_job, args)
 		
+		conn = db_setup.get_conn()
+		cur = conn.cursor()
 		update_options_chain(conn)
 		update_options_metrics(conn)
 		end = timer()
--- backend/src/scrape_oic.py
+++ backend/src/scrape_oic.py
@@ -6,6 +6,9 @@ import os
 import errno
 import numpy
 import pandas as pd
+import time
+
+DEBUG = False
 
 def get_cnt():
 	r = requests.get("https://www.optionseducation.org/toolsoptionquotes/optionsquotes")
@@ -13,17 +16,29 @@ def get_cnt():
 	return cnt.group().split("=")[1]
 
 def get_html(ticker, vdate):
-	cnt = get_cnt()
-	url = "https://oic.ivolatility.com/oic_adv_options.j?cnt="+cnt+"&ticker="+ticker+"&exp_date=-1"
-	print(url)
-	page = requests.get(url)
-	soup = BeautifulSoup(page.content, 'html.parser')
-	if not os.path.isdir("bs4_html"):
-		os.makedirs("bs4_html")
-	filename = os.path.join(os.getcwd(),"bs4_html/"+ticker+"/"+vdate+".html")
-	os.makedirs(os.path.dirname(filename), exist_ok=True)
-	with open(filename, "w", encoding='utf-8') as f:
-		f.write(str(soup))
+	try:
+		cnt = get_cnt()
+		url = "https://oic.ivolatility.com/oic_adv_options.j?cnt="+cnt+"&ticker="+ticker+"&exp_date=-1"
+		print(url)
+		exception = True
+		while(exception):
+			try:
+				page = requests.get(url)
+				soup = BeautifulSoup(page.content, 'html.parser')
+				exception = False
+			except requests.exceptions.RequestException as e:
+				print(e)
+				time.sleep(15)
+				exception = True
+		if not os.path.isdir("bs4_html"):
+			os.makedirs("bs4_html")
+		filename = os.path.join(os.getcwd(),"bs4_html/"+ticker+"/"+vdate+".html")
+		os.makedirs(os.path.dirname(filename), exist_ok=True)
+		with open(filename, "w", encoding='utf-8') as f:
+			f.write(str(soup))
+	except Exception as err:
+		print(err)
+
 
 def format_date(d):
 	d = datetime.strptime(d, '%b %d, %Y')
@@ -32,25 +47,13 @@ def format_date(d):
 #returns dataframe and the associated ticker (needs to be passed on for multiprocessing)
 def save_data(ticker, vdate):
 	try:
-		filename = os.path.join(os.getcwd(),"bs4_html/"+ticker+"/"+vdate+".html")
-		if not os.path.exists(filename):
-			get_html(ticker, vdate)
 		with open("bs4_html/"+ticker+"/"+vdate+".html", 'r') as f:
 			contents = f.read()
 			soup = BeautifulSoup(contents, 'lxml')
 		table = soup.find_all('table')
-		
-		
+
 		#return a list of dataframes made from the bs4 table query
 		df = pd.read_html(str(table), flavor='bs4', header=[1])
-		#with open("RAWHTML2.html", "w", encoding='utf-8') as f:
-			#f.write(str(table))
-			#pd.set_option("display.max_rows", None, "display.max_columns", None)
-			#f.write(str(pd.read_html(str(table), flavor='bs4', header=[0])))
-			#for i in range(len(df)):
-				#print(df[i].columns)
-			#	f.write(str(df[i].head()))
-
 
 		exp_dates = []
 		start, end = 0, 0
@@ -66,23 +69,25 @@ def save_data(ticker, vdate):
 				end += 1
 				df[i] = df[i][1:]
 				df[i].rename(columns=df[i].iloc[0])
+
 			elif re.search("Rho", headers):
 				if end == 0:
 					end = start
 				end +=1
 			else:
 				start +=1
+		#Can store historical volatility from last 2 tables
 		# print(df[-3])
 		# print(df[-2])
 		# print(df[-1])
 		pd.set_option("display.max_rows", 10, "display.max_columns", None)
-		file_path = "csv/" + ticker +"/"+ vdate+".csv"
+		directory = "csv/" + ticker
+		if not os.path.isdir(directory):
+			os.makedirs(directory)
+		file_path = directory+"/"+vdate+".csv"
+		if os.path.exists(file_path):
+			os.remove(file_path)
 		for i in range(start, end+1):
-			#directory = "csv/"+ticker+"/"+exp_dates[i-start]
-			if not os.path.isdir(directory):
-				os.makedirs(directory)
-			if os.path.exists(directory+"/"+vdate+".csv"):
-				continue
 			df[i]['Implied Vola%'] = df[i]['Implied Vola%'].str.rstrip('%')
 			df[i].drop(['Bid/Ask Mean', 'Change (%)'], axis=1, inplace=True)
 			df[i]['exp_date'] = exp_dates[i-start]
@@ -93,8 +98,12 @@ def save_data(ticker, vdate):
 									'Delta':'delta', 'Gamma':'gamma', 'Alpha':'alpha', 
 									'Vega':'vega', 'Rho':'rho', 
 									'Open Interest':'open_interest'}, inplace=True)
-
-			df[i].to_csv(directory+"/"+vdate+".csv", index=False)
+			if i == start:
+				df[i].to_csv(file_path, mode='a',index=False, header=True)
+				if DEBUG:
+					break
+			else:
+				df[i].to_csv(file_path, mode='a',index=False, header=False)
 	except Exception as err:
 		raise err
 
